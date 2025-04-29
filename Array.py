0002d9155fd99155f9d1c2aa0a5cea7152e6b269
Ipromise-Wei <ipromisewei@gmail.com>
2025-04-30 01:47:44 +0800

    修改数组文件

--- Array.py
+++ Array.py
@@ -22,3 +22,43 @@ def insert(nums: list[int], num: int , index: int):
     for i in range(len(nums)-1, index, -1):
         nums[i] = nums[i - 1]
     nums[index] = num
+    
+# 4.删除元素
+def remove(nums: list[int], index: int):
+    '''删除索引 index处的元素'''
+    # 把索引index之后的元素前移一位
+    for i in range(index, len(nums)-1):
+        nums[i] = nums[i + 1]
+        
+# 5.遍历数组
+def traverse(nums: list[int]):
+    '''遍历数组'''
+    count = 0
+    # 通过索引遍历数组
+    for i in range(len(nums)):
+        count += nums[i]
+    # 直接遍历数组元素
+    for num in nums:
+        count += num
+    # 同时遍历数组索引和元素
+    for i, num in enumerate(nums):
+        count += nums[i]
+        count += num
+        
+# 6.查找元素
+def find(nums: list[int], target: int) -> int:
+    '''在数组中查找target元素'''
+    for i in range(len(nums)):
+        if nums[i] == target:
+            return 1    
+    return -1
+
+# 7.扩容数组
+def extend(nums: list[int], enlarge: int) -> list[int]:
+    '''扩展数组长度'''
+    # 初始化一个扩展长度后的数组
+    res = [0] * (len(nums) + enlarge)
+    # 将原数组中的所有元素复制到新数组
+    for i in range(len(nums)):
+        res[i] = nums[nums]
+    return res
